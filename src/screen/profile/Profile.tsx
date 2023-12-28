ea5b3dc31ad068a5a27f57bcc044a96fe1fdadab
lilhuy0405 <luuhuy040501.travail@gmail.com>
2023-12-28 10:37:23 +0700

    update

--- src/screen/profile/Profile.tsx
+++ src/screen/profile/Profile.tsx
@@ -38,40 +38,41 @@ import UploadAvatarModal from '../../components/profile/UploadAvatarModal.tsx';
 import LoadingActivity from '../../components/common/loading/LoadingActivity.tsx';
 import ToastSuccessModal from '../../components/common/modal/ToastSuccessModal.tsx';
 import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5';
+
 const listMenu = [
   {
     name: 'Thông tin cá nhân',
-    icon: <FontAwesome5Icon name={'user'} color={'#CA1F24'} size={20} />,
+    icon: <FontAwesome5Icon name={'user'} color={'#CA1F24'} size={20}/>,
     screen: 'UserInfo',
   },
   {
     name: 'Quá trình công tác',
-    icon: <FontAwesome5Icon name={'edit'} color={'#CA1F24'} size={20} />,
+    icon: <FontAwesome5Icon name={'edit'} color={'#CA1F24'} size={20}/>,
     screen: 'WorkInfo',
   },
   {
     name: 'Lịch sử lương',
-    icon: <FontAwesome5Icon name={'coins'} color={'#CA1F24'} size={20} />,
+    icon: <FontAwesome5Icon name={'coins'} color={'#CA1F24'} size={20}/>,
     screen: 'SalaryInfo',
   },
   {
     name: 'Trang bị & Đào tạo',
-    icon: <BoxIcon width={20} height={20} color={'#CA1F24'} />,
+    icon: <BoxIcon width={20} height={20} color={'#CA1F24'}/>,
     screen: 'EducationInfo',
   },
   {
     name: 'Khen & Vi phạm',
-    icon: <FontAwesome5Icon name={'star'} color={'#CA1F24'} size={20} />,
+    icon: <FontAwesome5Icon name={'star'} color={'#CA1F24'} size={20}/>,
     screen: 'RewardAndPunishInfo',
   },
   {
     name: 'Nghỉ & Phép',
-    icon: <FontAwesome5Icon name={'minus'} color={'#CA1F24'} size={20} />,
+    icon: <FontAwesome5Icon name={'minus'} color={'#CA1F24'} size={20}/>,
     screen: 'AbsenceInfo',
   },
   {
     name: 'Cập nhật mật khẩu',
-    icon: <FontAwesome5Icon name={'cog'} color={'#CA1F24'} size={20} />,
+    icon: <FontAwesome5Icon name={'cog'} color={'#CA1F24'} size={20}/>,
     screen: 'SettingInfo',
   },
 ];
@@ -96,13 +97,12 @@ export default function Profile({navigation}: any) {
 
   const handleOpenUpdateLink = async () => {
     // Checking if the link is supported for links with custom URL scheme.
-    const supported = await Linking.canOpenURL(downloadUrl);
-
-    if (supported) {
-      // Opening the link with some app, if the URL scheme is "http" the web link should be opened
-      // by some browser in the mobile
+    // const supported = await Linking.canOpenURL(downloadUrl);
+    // console.log(downloadUrl);
+    try {
       await Linking.openURL(downloadUrl);
-    } else {
+    } catch (err: any) {
+      console.log(err);
       Alert.alert('Không thể mở liên kết');
     }
   };
@@ -144,7 +144,7 @@ export default function Profile({navigation}: any) {
   useRefreshOnFocus(refetch);
 
   if (isLoadingUserData) {
-    return <PrimaryLoading />;
+    return <PrimaryLoading/>;
   }
   return (
     userData && (
@@ -155,7 +155,7 @@ export default function Profile({navigation}: any) {
           onPress={() => {
             navigation.goBack();
           }}>
-          <ChevronLeft width={16} height={16} />
+          <ChevronLeft width={16} height={16}/>
         </TouchableOpacity>
         <ScrollView
           showsVerticalScrollIndicator={false}
@@ -179,7 +179,7 @@ export default function Profile({navigation}: any) {
                   }}
                 />
               ) : (
-                <NoAvatarIcon width={90} height={90} />
+                <NoAvatarIcon width={90} height={90}/>
               )}
             </View>
             <TouchableOpacity
@@ -187,7 +187,7 @@ export default function Profile({navigation}: any) {
               onPress={() => {
                 setIsOpenUploadAvatar(true);
               }}>
-              <CameraAvatarIcon width={30} height={30} />
+              <CameraAvatarIcon width={30} height={30}/>
             </TouchableOpacity>
           </View>
           <Text
@@ -216,11 +216,11 @@ export default function Profile({navigation}: any) {
 
           <View style={styles.ratingContainer}>
             <View style={styles.starContainer}>
-              <StarFillIcon width={16} height={16} />
-              <StarFillIcon width={16} height={16} />
-              <StarFillIcon width={16} height={16} />
-              <StarFillIcon width={16} height={16} />
-              <StarIcon width={16} height={16} />
+              <StarFillIcon width={16} height={16}/>
+              <StarFillIcon width={16} height={16}/>
+              <StarFillIcon width={16} height={16}/>
+              <StarFillIcon width={16} height={16}/>
+              <StarIcon width={16} height={16}/>
             </View>
             <Text style={[fs_12_400, text_black, {marginLeft: 10}]}>4.5</Text>
           </View>
@@ -243,7 +243,7 @@ export default function Profile({navigation}: any) {
             </View>
           </View>
 
-          <View style={styles.divider} />
+          <View style={styles.divider}/>
 
           <View>
             {listMenu.map((item, index) => (
@@ -266,7 +266,7 @@ export default function Profile({navigation}: any) {
                     {item.name}
                   </Text>
                 </View>
-                <ChevronRightProfileIcon width={16} height={16} />
+                <ChevronRightProfileIcon width={16} height={16}/>
               </TouchableOpacity>
             ))}
           </View>
@@ -288,13 +288,14 @@ export default function Profile({navigation}: any) {
           handleUploadAvatar={handleUploadAvatar}
         />
 
-        <LoadingActivity isLoading={isLoading} />
+        <LoadingActivity isLoading={isLoading}/>
 
         <ToastSuccessModal
           visible={isSuccess}
           handlePressOk={() => setIsSuccess(false)}
           description={'Cập nhật avatar thành công'}
         />
+
       </SafeAreaView>
     )
   );
